20841601fd20543a7a5ca776a863e1eae504752a
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2025-07-10 12:48:09 +0530

    Create Student model

---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Management.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name = "Student ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Student Name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
