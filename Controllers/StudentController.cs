fbcfb56943e641896cae2fcef29b6c385436ca2a
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2025-07-10 13:48:35 +0530

    add student controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student_Management.Data;
+using Student_Management.Models;
+
+namespace Student_Management.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Student> students =_context.Students.ToList(); 
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+    }
+}
