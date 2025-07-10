31ce743981fd5a2afc5691cd4966502651726e23
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2025-07-10 13:17:13 +0530

    Create AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management.Models;
+
+namespace Student_Management.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
